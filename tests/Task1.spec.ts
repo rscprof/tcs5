2e616bad4602ac80c40024f37b4a39acf631c1b6
rsc@rsc.pp.ru <rsc@rsc.pp.ru>
2023-12-24 09:30:48 +0300

    Accept message before throw

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -4,6 +4,7 @@ import { Task1 } from '../wrappers/Task1';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
+
 describe('Task1', () => {
     let code: Cell;
 
@@ -17,9 +18,10 @@ describe('Task1', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        task1 = blockchain.openContract(Task1.createFromConfig({}, code));
-
         const deployer = await blockchain.treasury('deployer');
+
+        task1 = blockchain.openContract(Task1.createFromConfig({address: deployer.address}, code));
+
 
         const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
 
@@ -35,4 +37,57 @@ describe('Task1', () => {
         // the check is done inside beforeEach
         // blockchain and task1 are ready to use
     });
+
+    it('should return correct number from get method get_seqno', async () => {
+        const caller = await blockchain.treasury('caller');
+        expect(await task1.getSeqno()).toEqual(123);
+    });
+
+    it('should return correct number from get method get_execution_time', async () => {
+        const caller = await blockchain.treasury('caller');
+        expect(await task1.getExecutionTime()).toEqual(199);
+    });
+
+    it('should correct test seq_no', async () => {
+  
+        const res = await task1.sendUpdate(
+            BigInt(0),
+            BigInt(0),
+            BigInt(0),
+            BigInt(123)
+        );  // performing an action with contract main and saving result in res
+
+        expect(res.transactions).toHaveTransaction({                             // configure the expected result with expect() function
+            exitCode: 119                                                        // set the desirable result using matcher property success
+        });
+    
+        //printTransactionFees(res.transactions); 
+        // const caller = await blockchain.treasury('caller');
+        // expect(await task1.getExecutionTime()).toEqual(199);
+    });
+
+    
+
+
+
+    
+
+    // it('get seq_no', async () => {
+    //     blockchain = await Blockchain.create();
+
+    //     task1 = blockchain.openContract(Task1.createFromConfig({}, code));
+
+    //     const deployer = await blockchain.treasury('deployer');
+
+    //     const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+    //     expect(deployResult.transactions).toHaveTransaction({
+    //         from: deployer.address,
+    //         to: task1.address,
+    //         deploy: true,
+    //         success: true,
+    //     });
+        
+    // });
+
 });
--- wrappers/Task1.ts
+++ wrappers/Task1.ts
@@ -1,13 +1,36 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, ExternalAddress, Sender, SendMode } from 'ton-core';
+import { Maybe } from 'ton-core/dist/utils/maybe';
 
-export type Task1Config = {};
+export type Task1Config = {
+    address: Maybe<Address | ExternalAddress>;
+};
 
+// public_key: uint256
+// execution_time: uint32
+// receiver: MsgAddressInt
+// seqno: uint32
 export function task1ConfigToCell(config: Task1Config): Cell {
-    return beginCell().endCell();
+    return beginCell()
+    .storeInt(0,256) //public_key
+    .storeInt(199,32) //exec time
+    .storeAddress(config.address) //receiver
+    .storeInt(123,32) //seqno
+    .endCell();
 }
 
 export class Task1 implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    async getSeqno(provider: ContractProvider) {
+        const result = (await provider.get('get_seqno', [])).stack;
+        return result.readNumber();
+    }
+
+    async getExecutionTime(provider: ContractProvider) {
+        const result = (await provider.get('get_execution_time', [])).stack;
+        return result.readNumber();
+    }
+
 
     static createFromAddress(address: Address) {
         return new Task1(address);
@@ -26,4 +49,20 @@ export class Task1 implements Contract {
             body: beginCell().endCell(),
         });
     }
+
+    //update#9df10277 query_id:uint64 signature:bits512 ^[ locked_for:uint32 new_seqno:uint32 ] = ExtInMsgBody
+    async sendUpdate(
+        provider: ContractProvider, 
+        queryId: bigint,
+        signature: bigint,
+        locked_for: bigint,
+        new_seqno: bigint
+        ) {
+        await provider.external(
+            beginCell().storeUint(0x9df10277,32).storeUint(queryId,64).storeUint(signature,512).storeRef(
+                beginCell().storeUint(locked_for,32).storeUint(new_seqno,32).endCell()
+            ).endCell()
+            );
+    }
+
 }
