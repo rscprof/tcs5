a37d8536f3682ce3a42aa538cb9221423da54ca8
rsc@rsc.pp.ru <rsc@rsc.pp.ru>
2023-12-26 09:46:36 +0300

    Experience with dict

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Cell, Dictionary, Slice, beginCell, toNano } from 'ton-core';
 import { Task3 } from '../wrappers/Task3';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
@@ -16,6 +16,12 @@ describe('Task3', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+        blockchain.verbosity = {
+            blockchainLogs: false,
+            vmLogs : 'vm_logs',
+            debugLogs: true,
+            print: true
+        }
 
         task3 = blockchain.openContract(Task3.createFromConfig({}, code));
 
@@ -29,10 +35,92 @@ describe('Task3', () => {
             deploy: true,
             success: true,
         });
+         
+        const user = await blockchain.treasury('user');
+
+
+        const sendFirstResult = await task3.sendFirstMessage(user.getSender());
+
+        expect(sendFirstResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: task3.address,
+            success: true,
+        })
+
+        const number = await task3.getVersion();
+
+        expect(number).toEqual(1);
+
     });
 
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task3 are ready to use
     });
+
+
+    it('should correct vork with version 2', async () => {
+        // the check is done inside beforeEach
+        // blockchain and task3 are ready to use
+   
+        const user = await blockchain.treasury('user');
+
+        const sendResult = await task3.sendVersionMessage(user.getSender(),BigInt(2));
+
+        expect(sendResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: task3.address,
+            success: false,
+            exitCode: 200,
+        })
+
+
+    });
+
+    it('should correct vork with version 2 with code', async () => {
+        // the check is done inside beforeEach
+        // blockchain and task3 are ready to use
+   
+        const user = await blockchain.treasury('user');
+
+        const sendResult = await task3.sendVersionMessageWithCode(user.getSender(),BigInt(2),Cell.EMPTY);
+
+        expect(sendResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: task3.address,
+            success: false,
+            exitCode: 400,
+        })
+
+
+    });
+
+    it('should correct vork with version 2 with code and dict', async () => {
+        // the check is done inside beforeEach
+        // blockchain and task3 are ready to use
+   
+        const user = await blockchain.treasury('user');
+
+        const dict = Dictionary.empty<number,Cell>(Dictionary.Keys.Uint(32),Dictionary.Values.Cell());
+    
+        
+
+        dict.set(1,beginCell().storeUint(2,32).storeMaybeRef(null).endCell());
+
+        const sendResult = await task3.sendVersionMessageWithCodeAndDict(user.getSender(),BigInt(2),Cell.EMPTY,dict);
+
+        expect(sendResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: task3.address,
+            success: true,
+        })
+
+
+        const number = await task3.getVersion();
+
+        expect(number).toEqual(2);
+    });
+
+
+
 });
--- wrappers/Task3.ts
+++ wrappers/Task3.ts
@@ -1,4 +1,4 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Dictionary, Sender, SendMode, Slice, toNano } from 'ton-core';
 
 export type Task3Config = {};
 
@@ -26,4 +26,67 @@ export class Task3 implements Contract {
             body: beginCell().endCell(),
         });
     }
+    
+
+    async sendFirstMessage(
+        provider: ContractProvider, 
+            user: Sender, 
+            ) {    
+            await provider.internal(user, {
+                    value: toNano('0.01'),
+                    sendMode: SendMode.PAY_GAS_SEPARATELY,
+                    body: beginCell().storeUint(0,32).storeMaybeRef(null).endCell()
+                }
+            );
+    }
+
+    async getVersion(provider: ContractProvider) {
+        const result = (await provider.get('version', [])).stack;
+        return result.readNumber();
+    }
+
+    async sendVersionMessage(
+        provider: ContractProvider, 
+            user: Sender, 
+            version: bigint,
+            ) {    
+            await provider.internal(user, {
+                    value: toNano('0.01'),
+                    sendMode: SendMode.PAY_GAS_SEPARATELY,
+                    body: beginCell().storeUint(version,32).endCell()
+                }
+            );
+    }
+
+    async sendVersionMessageWithCode(
+        provider: ContractProvider, 
+            user: Sender, 
+            version: bigint,
+            code: Cell,
+            ) {    
+            await provider.internal(user, {
+                    value: toNano('0.01'),
+                    sendMode: SendMode.PAY_GAS_SEPARATELY,
+                    body: beginCell().storeUint(version,32).storeMaybeRef(code).endCell()
+                }
+            );
+    }
+
+    async sendVersionMessageWithCodeAndDict(
+        provider: ContractProvider, 
+            user: Sender, 
+            version: bigint,
+            code: Cell,
+            dict: Dictionary<number,Cell>,
+            ) {    
+            await provider.internal(user, {
+                    value: toNano('0.01'),
+                    sendMode: SendMode.PAY_GAS_SEPARATELY,
+                    body: beginCell().storeUint(version,32).storeMaybeRef(code).storeDict(dict).storeRef(beginCell().endCell()).endCell()
+                }
+            );
+    }
+
+
+    
 }
