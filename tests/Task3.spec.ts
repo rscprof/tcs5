3ef85617a9a65d7c56d0601bd79e6b53909181d5
rsc@rsc.pp.ru <rsc@rsc.pp.ru>
2023-12-27 12:35:52 +0300

    Fix cell error

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -1,8 +1,9 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, Dictionary, Slice, beginCell, toNano } from 'ton-core';
+import { BitString, Builder, Cell, Dictionary, Slice, beginCell, toNano } from 'ton-core';
 import { Task3 } from '../wrappers/Task3';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import {compileFunc, CompilerConfig} from "@ton-community/func-js";
 
 describe('Task3', () => {
     let code: Cell;
@@ -16,12 +17,12 @@ describe('Task3', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        // blockchain.verbosity = {
-        //     blockchainLogs: false,
-        //     vmLogs : 'vm_logs',
-        //     debugLogs: true,
-        //     print: true
-        // }
+        blockchain.verbosity = {
+            blockchainLogs: false,
+            vmLogs : 'vm_logs',
+            debugLogs: true,
+            print: true
+        }
 
         task3 = blockchain.openContract(Task3.createFromConfig({}, code));
 
@@ -98,14 +99,29 @@ describe('Task3', () => {
     it('should correct vork with version 2 with code and dict', async () => {
         // the check is done inside beforeEach
         // blockchain and task3 are ready to use
+
+        let result = await compileFunc({
+            // Targets of your project
+            targets: ['descriptions/3-example/counter-mig-v2-v3.fc'],
+            // Sources
+            sources: {
+                "contracts/imports/stdlib.fc": "<stdlibCode>",
+                "descriptions/3-example/counter-mig-v2-v3.fc": "<contractCode>",
+                // The rest of the files which are included in main.fc if any
+            }
+        });
+    
    
         const user = await blockchain.treasury('user');
 
-        const dict = Dictionary.empty<number,Cell>(Dictionary.Keys.Uint(32),Dictionary.Values.Cell());
+       // const dict = Dictionary.empty<number,Slice>(Dictionary.Keys.Uint(32),Dictionary.Values.);
     
         
+        
 
-        dict.set(1,beginCell().storeUint(2,32).storeMaybeRef(null).endCell());
+        //dict.set(1,
+            
+          //  beginCell().storeUint(2,32).storeMaybeRef(null).endCell());
 
         const sendResult = await task3.sendVersionMessageWithCodeAndDict(user.getSender(),BigInt(2),Cell.EMPTY,dict);
 
